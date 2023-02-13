2f58dcb38384ca378c934f92db898c7589d89edd
madhur999 <madhurgupta@fabloplatforms.com>
2023-02-13 10:42:01 +0530

    changes in all bookings

--- src/api/v1/routes/booking.route.js
+++ src/api/v1/routes/booking.route.js
@@ -8,7 +8,7 @@ const { authenticateUser } = require('../middlewares/authToken');
 
 router.post('/createBooking', authenticateUser, createBooking)
 router.get('/getAllSlots', authenticateUser, getAllSlots)
-router.get('/allBookings', authenticateUser, allBookings)
+router.get('/allBookings',  allBookings)
 router.get('/bookingByUser', authenticateUser, allBookingUser)
 router.post('/rejected/:bookingId', authenticateUser, bookingRejected)
 router.post('/purchase', authenticateUser, purchaseBooking)
